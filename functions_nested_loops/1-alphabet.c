989ebc1ff321cb12732584f7527478f9411a54c2
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-21 13:42:23 +0100

    done

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+# include "main.h"
+/**
+ * print_alphabet - entry point
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char lowercase = 'a';
+
+	for (; lowercase <= 'z'; lowercase++)
+	{
+	_putchar(lowercase);
+}
+	_putchar('\n');
+}
