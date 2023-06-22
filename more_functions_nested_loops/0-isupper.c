0c5d53bca26f9a245d9fb0f4267d1468e9d74ff8
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-22 18:32:05 +0100

    isupper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * @c: lowecase
+ * Return: 1 if c is lowercase otherwise 0
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
