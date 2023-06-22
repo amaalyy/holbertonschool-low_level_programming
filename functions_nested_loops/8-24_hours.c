74abd4ac235300174212d3614967cdf114b1c73d
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-22 13:46:49 +0100

    every minute of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,9 +7,10 @@
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h <= 23; h++)
 	{
-		for(m = 0; m <= 59; m++)
+		for (m = 0; m <= 59; m++)
 		{
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
@@ -19,5 +20,4 @@ void jack_bauer(void)
 			_putchar('\n');
 		}
 	}
-	
 }
