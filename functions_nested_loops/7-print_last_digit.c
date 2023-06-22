4ee873ad09230f5fcf2ca41b5774867c7a941b62
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-22 12:07:52 +0100

    print_last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *@n: print_sign
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+if (n > 0)
+{
+	return (n);
+}
+else
+{
+	n = -1 * (n % 10);
+	_putchar(n + '0');
+	return (n);
+}
+}
