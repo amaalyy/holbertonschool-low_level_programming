876e1a522935ebf7f291655732d28b8ddad32789
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-22 19:47:10 +0100

    multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: 1 if c is lowercase otherwise 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
