a7cf7ad12ba545f14815c905e0b00f97bf16382e
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-23 21:18:19 +0100

    a

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,7 +1,28 @@
 #include "main.h"
 /**
- * print_diagonal - 
+ * print_diagonal - draws a diagonal line 
  * @n:  number of times the character
  * Return: 0
  */
 void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+		else
+		{
+			int i, v; 
+
+			for (i = 0; i < n; i++)
+			{
+				for (v = 0; v < n; v++)
+				
+					if (v == i)
+					_putchar('\\');
+					else if (v < i)
+						_putchar(' ');	
+			}
+				_putchar('\n');
+		}
+}
