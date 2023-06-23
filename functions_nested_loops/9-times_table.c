8cf1c0ff8581e45f6e9087466d4346e83e2cd8a1
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-23 14:21:33 +0100

    9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * times_table - entry point
+ * Return: 0
+ */
+void times_table(void)
+{
+	int r, m, i;
+
+	for (m = 0; m <= 9; m++)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		r = m * i;
+		if (r >= 10)
+		{
+			_putchar(r / 10 + '0');
+			_putchar(r % 10 + '0');
+			if (r >= 10)
+			{
+			_putchar(' ');
+			}
+		}
+		else
+		{
+			_putchar(r + '0');
+			_putchar(' ');
+			_putchar(' ');
+		}
+			if (i < 9)
+		{
+			_putchar(',');
+		}
+	}
+			_putchar ('\n');
+}
+}
