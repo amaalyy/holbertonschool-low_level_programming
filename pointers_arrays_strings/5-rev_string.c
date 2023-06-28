e00baebecc2779f26ae21b1e4c6043636cc8a3c4
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-28 17:44:15 +0100

    function that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - function reverses a string
+ * @s: pointer
+ * Return: 0
+*/
+void rev_string(char *s)
+{
+	int rev;
+	char empty;
+	int first;
+
+	for (rev = 0; s[rev] != '\0'; rev++)
+	{
+	}
+	rev = rev - 1;
+
+	for (first = 0; first < rev / 2; first++, rev--)
+	{
+		empty = s[first];
+		s[first] = s[rev];
+		s[rev] = empty;
+	}
+}
