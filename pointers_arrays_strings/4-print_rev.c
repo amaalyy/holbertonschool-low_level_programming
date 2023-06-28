c95e29f89abc0687b556ecab4f238501aefb08a5
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-28 14:43:57 +0100

    string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev -  prints a string in reverse
+ * @s: poniter
+ * Return: 0
+*/
+void print_rev(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+	}
+	for (; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+		_putchar('\n');
+}
