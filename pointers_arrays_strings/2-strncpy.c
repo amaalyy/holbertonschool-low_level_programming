e335a2220792b06556b21ae06dfb9c3bfbe72c02
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-06-30 12:32:10 +0100

    copies a string

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *_strncpy - copy a string
+ * @dest: first string
+ * @src: second string
+ * @n: maximum number of chars
+ * Return: 0
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && n > i; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (n > i)
+	{
+		dest[i] = '\0';
+	}
+		return (dest);
+}
