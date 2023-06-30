666940ff218a6e0095415a29bb3ceec7ee20ba19
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-07-01 00:02:56 +0100

    capitalizes all words

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,7 +13,7 @@ void reverse_array(int *a, int n)
 	for (i = 0; i < n / 2; i++)
 	{
 		empty = a[i];
-		a[i] = a[n - i - 1];
+		a[i] = a[n - i w- 1];
 		a[n - i - 1] = empty;
 	}
 }
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * *cap_string - capitalizes all words
+ * @str: pointer
+ * Return: str
+*/
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char arr[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"','(', ')', '{', '}'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; arr[j] != '\0'; j++)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				if (arr[j] == str[i - 1])
+				{
+					str[i] = str[i] - 32;
+				}
+			}									
+		}
+	}
+	return (str);
+}
+                        
