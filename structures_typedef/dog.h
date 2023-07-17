10601c7b24c7bbee00190d9e48864855ef27f679
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-07-17 18:13:07 +0100

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef Poppy
+#define Poppy
+/**
+* struct dog - struct
+* @name: dog name
+* @age : dog age
+* @owner: dog owner 
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif
