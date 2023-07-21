7b50978135eadba7f63269905a8a0b0738a2b9d9
Amal <amal@MacBook-Pro-alkhas-b-Amal.local>
2023-07-21 10:54:51 +0100

    sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,13 +3,12 @@
  * sum_them_all - function that returns the sum of all its parameters
  * @n: int
  * Return: sum
- * 
 */
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, sum;
 	va_list ptr;
-	
+
 	va_start(ptr, n);
 	if (n == 0)
 	{
