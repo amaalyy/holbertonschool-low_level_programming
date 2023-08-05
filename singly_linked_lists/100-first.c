a340327478d2adb77d7f514ddf712a2c82c6b8ec
amaalyy <amal.elzowawi@gmail.com>
2023-08-05 20:05:43 +0100

    test

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ *holberton_main - Print text before main func is executed
+ */
+void holberton_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
