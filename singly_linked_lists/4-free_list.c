f5ec6a1e820f9f0d1d0f9bf072e7724cd1052f9b
amaalyy <amal.elzowawi@gmail.com>
2023-08-05 19:58:59 +0100

    test

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: pointer to the head
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head->next;
+		free(head->str);
+		free(head);
+		head = ptr;
+	}
+}
