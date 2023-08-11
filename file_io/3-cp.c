a5bbeedd61de2ae0ab3be50ab6bb1af1e5c96fa0
amaalyy <amal.elzowawi@gmail.com>
2023-08-11 23:26:59 +0100

    done

---
+++ file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: copies the content of a file to another file
+ * @argc: arguments
+ * @argv: arguments
+ * Return:0
+*/
+int main(int argc, char **argv)
+{
+    int f, f1, i = 0;
+    char *bf;
+
+    if (argc > 3)
+    {
+        printf("Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    f = open(argv[1], O_RDONLY);
+    if (f == -1)
+    {
+        printf("Error: Can't read from file NAME_OF_THE_FILE\n");
+        exit(98);
+    }
+    f1 = open(argv[2], O_WRONLY | O_TRUNC);
+    if (f1 == -1)
+    {
+        f1 = open(argv[2], O_CREAT | O_WRONLY, 0664);
+        if (f1 == -1)
+        {
+            printf("Error: Can't write to NAME_OF_THE_FILE\n");
+            exit(98);
+        }
+    }
+    bf = malloc(1024);
+    read(f, bf, 1024);
+    while (*bf != EOF)
+    {
+        i = i + 1024;
+        bf = realloc(bf, i);
+        read(f, bf, i);
+    }
+    dprintf(f1, "%s", bf);
+    return(0);
+}
