db2b08a3468afe6c8a5cea8f818513c346ec3b2d
amaalyy <amal.elzowawi@gmail.com>
2023-08-12 00:00:28 +0100

    done

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ *hash_table_create - creating a hash table
+ *@size: the size of the hash table
+ *Return: pointer to the newly created hash table
+ *If something went wrong return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new = NULL;
+	size_t i;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->array = malloc(sizeof(hash_node_t *) * size);
+	if (new->array == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		new->array[i] = NULL;
+	}
+	new->size = size;
+	return (new);
+}
