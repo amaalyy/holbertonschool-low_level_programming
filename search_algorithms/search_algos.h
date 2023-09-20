e49d9eeabbfddc6abea8d19d1fee1537d96b9f6c
amaalyy <amal.elzowawi@gmail.com>
2023-09-20 14:26:03 +0100

    header file

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
