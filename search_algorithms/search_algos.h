fae13499c00fdd64e4d30d031d98f59956c660a9
amaalyy <amal.elzowawi@gmail.com>
2023-09-22 19:41:48 +0100

    add prototype

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -10,5 +10,6 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int bsearch(int *array, int start, int size, int value);
 
 #endif
