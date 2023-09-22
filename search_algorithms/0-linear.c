f03afd5bafa7cd367c1c6abc946ca465ee7c7693
amaalyy <amal.elzowawi@gmail.com>
2023-09-22 16:49:56 +0100

    fix error

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -10,17 +10,20 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	while (i <= size - 1)
+	if (array)
 	{
-		if (value != array[i])
+		while (i <= size - 1)
 		{
-			printf ("Value checked array[%ld] = [%d]\n", i, array[i]);
-			i++;
-		}
-		if (value == array[i])
-		{
-			printf ("Value checked array[%ld] = [%d]\n", i, array[i]);
-			return (i);
+			if (value != array[i])
+			{
+				printf ("Value checked array[%ld] = [%d]\n", i, array[i]);
+				i++;
+			}
+			if (value == array[i])
+			{
+				printf ("Value checked array[%ld] = [%d]\n", i, array[i]);
+				return (i);
+			}
 		}
 	}
 	return (-1);
